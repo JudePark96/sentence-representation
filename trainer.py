2dda319f5c1afb49ed17a3e8743a57ba65764fcd
JudePark96 <judepark@kookmin.ac.kr>
2020-04-07 15:10:30 +0900

    added summary writer

--- trainer.py
+++ trainer.py
@@ -2,10 +2,11 @@ __author__ = 'JudePark'
 __email__ = 'judepark@kookmin.ac.kr'
 
 
-from transformers import BertModel, BertTokenizer
-from torch.utils.data import Dataset, DataLoader
 from bookcorpus_dataset import get_data_loader, BookCorpusDataset
+from transformers import BertModel, BertTokenizer
 from config import get_device_setting, data_config
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import Dataset, DataLoader
 from model import BertSE
 from tqdm import tqdm
 
@@ -21,12 +22,15 @@ class Trainer(object):
         self.model = model.to(self.device)
         self.epoch = epoch
         self.optimizer = optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()), lr=1e-5)
-        self.loss = nn.KLDivLoss(reduction='batchmean')
+        self.loss = nn.KLDivLoss(reduction='batchmean').to(self.device)
+        self.writer = SummaryWriter()
 
 
     def train(self, train_loader: DataLoader):
+        steps = 0
         for iter in tqdm(range(self.epoch)):
             for idx, input_ids in tqdm(enumerate(train_loader)):
+                steps += 1
                 self.optimizer.zero_grad()
                 input_ids = input_ids.to(self.device)
                 bs, seq_len = input_ids.size()
@@ -40,13 +44,15 @@ class Trainer(object):
                 nn.utils.clip_grad_norm_(filter(lambda p: p.requires_grad, self.model.parameters()), 2.0)
                 self.optimizer.step()
 
-                if (idx + 1) % 100 == 0:
+                if (steps) % 100 == 0:
+                    self.writer.add_scalar('kl_loss/train', kl_loss.item(), steps)
                     print(f'kl_loss: {kl_loss}')
 
-                if (idx + 1) % 1000 == 0:
+                if (steps + 1) % 1000 == 0:
                     T.save(self.model.state_dict(), './checkpoint/' + f'model-{iter}-{idx}.pt')
 
     def evaluate(self):
+
         pass
 
 
