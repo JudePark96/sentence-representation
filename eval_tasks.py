26ee289e201a6cfa937d290f9c0182be4c0e1d80
JudePark96 <judepark@kookmin.ac.kr>
2020-04-07 21:00:10 +0900

    added evaluation

--- eval_tasks.py
+++ eval_tasks.py
@@ -1,44 +1,50 @@
 __author__ = 'JudePark'
 __email__ = 'judepark@kookmin.ac.kr'
 
-from typing import Tuple
 
+from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from transformers import BertTokenizer
+from config import get_device_setting
 from eval_dataset import load_data
 from data_utils import tokenize
+from model import BertSE
+from typing import Tuple
 from tqdm import tqdm
 
 
 import torch.nn as nn
 import numpy as np
-
-def encode(tokenizer: BertTokenizer, texts: list, labels: list) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Encoding the eval task dataset
-    :param tokenizer:
-    :param text:
-    :param labels:
-    :return:
-    """
-
-    texts = np.array([tokenize(tokenizer, text, 50)['input_ids'] for text in tqdm(texts)])
-    labels = np.array(labels)
-
-    print(texts[0])
-    print(texts[1])
-    print(texts[2])
-    return texts, labels
+import torch
 
 
-def extract_features(encoder: nn.Module, texts: np.ndarray, labels) -> None:
+def extract_features(encoder: BertSE, texts: np.ndarray) -> np.ndarray:
+    encoder.eval()
+
+    # [bs x embed_dim(hid_dim)]
+    sentence_embedding = encoder(torch.from_numpy(texts).long().to(get_device_setting()), is_eval=True)
+
+    encoder.train()
+
+    # scikit-learn is avaliable at CPU operation.
+    return sentence_embedding.cpu().detach().numpy()
 
 
-    pass
+def fit_lr(train_x: np.ndarray, train_y: np.ndarray, test_x: np.ndarray, test_y: np.ndarray,
+                            random_state:int, c: float) -> Tuple[float, float]:
+
+    lr = LogisticRegression(random_state=random_state, C=c)
+    lr.fit(train_x, train_y)
+    acc = lr.score(test_x, test_y)
+
+    pred_y = lr.predict(test_x)
+    f1 = f1_score(test_y, pred_y)
+
+    return acc, f1
 
 
 if __name__ == '__main__':
     texts, labels = load_data('MPQA', './rsc/eval_data/mpqa.all')
-    encode(BertTokenizer.from_pretrained('bert-base-uncased'), texts, labels)
+    # encode(BertTokenizer.from_pretrained('bert-base-uncased'), texts, labels)
     pass
--- trainer.py
+++ trainer.py
@@ -3,10 +3,12 @@ __email__ = 'judepark@kookmin.ac.kr'
 
 
 from bookcorpus_dataset import get_data_loader, BookCorpusDataset
-from transformers import BertModel, BertTokenizer
+from eval_tasks import extract_features, fit_lr
 from config import get_device_setting, data_config
+from transformers import BertModel, BertTokenizer
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import Dataset, DataLoader
+from eval_dataset import load_data
 from model import BertSE
 from tqdm import tqdm
 
@@ -17,10 +19,11 @@ import torch as T
 
 
 class Trainer(object):
-    def __init__(self, model: BertSE, epoch:int) -> None:
+    def __init__(self, model: BertSE, tokenizer: BertTokenizer, epoch:int) -> None:
         self.device = get_device_setting()
         self.model = model.to(self.device)
         self.epoch = epoch
+        self.tokenizer = tokenizer
         self.optimizer = optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()), lr=1e-5)
         self.loss = nn.KLDivLoss(reduction='batchmean').to(self.device)
         self.writer = SummaryWriter()
@@ -44,16 +47,25 @@ class Trainer(object):
                 nn.utils.clip_grad_norm_(filter(lambda p: p.requires_grad, self.model.parameters()), 2.0)
                 self.optimizer.step()
 
+                # TODO => 잘되나 확인
+                self.evaluate(steps=steps)
+
                 if (steps) % 100 == 0:
-                    self.writer.add_scalar('kl_loss/train', kl_loss.item(), steps)
+                    self.writer.add_scalar('Train/KL_Loss', kl_loss.item(), steps)
                     print(f'kl_loss: {kl_loss}')
 
                 if (steps + 1) % 1000 == 0:
                     T.save(self.model.state_dict(), './checkpoint/' + f'model-{iter}-{idx}.pt')
 
-    def evaluate(self):
+    def evaluate(self, steps: int) -> None:
+        train_x, test_x, train_y, test_y = load_data('MPQA', './rsc/eval_data/mpqa.all', tokenizer)
+        train_x_embedding = extract_features(self.model, train_x)
+        test_x_embedding = extract_features(self.model, test_x)
 
-        pass
+        acc, f1 = fit_lr(train_x_embedding, train_y, test_x_embedding, test_y, random_state=49, c=1.0)
+
+        self.writer.add_scalar('Eval/MRQA_acc', acc, steps)
+        self.writer.add_scalar('Eval/MRQA_f1', f1, steps)
 
 
 if __name__ == '__main__':
@@ -72,6 +84,6 @@ if __name__ == '__main__':
     train_loader = get_data_loader(dataset, 32)
     model = BertSE(bert_model, False)
 
-    Trainer(model, 5).train(train_loader)
+    Trainer(model, tokenizer, 5).train(train_loader)
 
     pass
